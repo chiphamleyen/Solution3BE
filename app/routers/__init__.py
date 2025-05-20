d90e217811dd200bb9cf8f4691a82f9634722854
bao <tienbaoams@gmail.com>
2025-05-20 13:38:17 +1000

    route malware profile api

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -4,6 +4,7 @@ import app.routers.account as account
 import app.routers.history as history
 import app.routers.prediction as prediction
 import app.routers.report as report
+import app.routers.malware_profile as malware_profile
 
 def add_route(route, routers, tags):
     prefix = '/api'
@@ -23,3 +24,4 @@ add_route(account.router, routers, account.router.tags)
 add_route(history.router, routers, history.router.tags)
 add_route(prediction.router, routers, prediction.router.tags)
 add_route(report.router, routers, report.router.tags)
+add_route(malware_profile.router, routers, malware_profile.router.tags)
